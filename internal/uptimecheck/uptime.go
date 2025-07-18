d442c4beedf74d4e81ab8176b7be7d258b07f630
atharv <atharv55215@gmail.com>
2025-07-18 23:50:34 +0530

    Adding basic upcheck code

---
+++ internal/uptimecheck/uptime.go
@@ -0,0 +1,25 @@
+package uptimecheck
+
+import (
+	"log/slog"
+	"net/http"
+	"net/url"
+
+	"github.com/atharv3221/uptimecheck/internal/response"
+)
+
+func UptimeCheck(request string) {
+	resp, err := http.Get(request)
+
+	if err != nil {
+		if err, ok := err.(*url.Error); ok {
+			output := response.URLError(err)
+			slog.Error("error occured", "response", output)
+			return
+		}
+		output := response.URLErr(err, resp)
+		slog.Error("error occured", "response", output)
+		return
+	}
+	slog.Info("Successful", "response", response.GenerateResponse(resp))
+}
